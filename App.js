71e09c75627b822ce624e5bf8c21d5dfddea46d5
mr-bmv <mr.m.botkin@gmail.com>
2020-12-15 11:29:01 +0300

    List of Posts

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, FlatList } from 'react-native';
 import { FontAwesome, FontAwesome5 } from '@expo/vector-icons'
 import Constants from 'expo-constants';
 
 import HomeScreen from './src/screens/Home'
 import Post from './src/components/Post';
+import feed from './assets/data/feed'
+import SearchResultsScreen from './src/screens/SearchResults';
 
 export default function App() {
+
   return (
-    <View style={{ paddingTop: Constants.statusBarHeight, }}>
+    <View style={{ paddingTop: Constants.statusBarHeight, flex: 1 }}>
       <StatusBar
         barStyle="dark-content"
       // hidden={true}
       />
       <SafeAreaView>
         {/* <HomeScreen /> */}
-        <Post />
+        <SearchResultsScreen />
       </SafeAreaView>
     </View>
   );
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -4,40 +4,40 @@ import styles from './styles.js';
 
 const Post = (props) => {
 
-  const post = props.post;
+  const { image, bed, bedroom, type, title, oldPrice, newPrice, totalPrice } = props.post;
 
   return (
     <View style={styles.container}>
       {/* Image  */}
       <Image
         style={styles.image}
-        source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgMTQoDgeZfxzECUiylW5kbLUBPtAQBfERGQ&usqp=CAU" }}
+        source={{ uri: image }}
       />
 
       {/* Bed & Bedroom  */}
       <Text style={styles.bedrooms}>
-        {/* {post.bed} bed {post.bedroom} bedroom */}
-        1 bed 1 bedroom
+        {bed} bed {bedroom} bedroom
+        {/* 1 bed 1 bedroom */}
       </Text>
 
       {/* Type & Description */}
       <Text style={styles.description} numberOfLines={3}>
-        {/* {post.type}. {post.title} */}
-        Lorem • Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis commodi blanditiis magni ducimus repellat nesciunt necessitatibus exercitationem natus, eum culpa?
+        {type} • {title}
+        {/* Lorem • Lorem20 */}
       </Text>
 
       {/*  Old price & new price */}
       <Text style={styles.prices}>
-        {/* <Text style={styles.oldPrice}>${post.oldPrice}</Text> */}
-        {/* <Text style={styles.price}>  ${post.newPrice} </Text> */}
-        <Text style={styles.oldPrice}>$20</Text>
-        <Text style={styles.price}>  $15 </Text>
+        <Text style={styles.oldPrice}>${oldPrice}</Text>
+        <Text style={styles.price}>  ${newPrice} </Text>
+        {/* <Text style={styles.oldPrice}>$20</Text> */}
+        {/* <Text style={styles.price}>  $15 </Text> */}
         / night
       </Text>
 
       {/*  Total price */}
-      {/* <Text style={styles.totalPrice}>${post.totalPrice} total</Text> */}
-      <Text style={styles.totalPrice}>$20 total</Text>
+      <Text style={styles.totalPrice}>${totalPrice} total</Text>
+      {/* <Text style={styles.totalPrice}>$20 total</Text> */}
     </View>
 
   );
